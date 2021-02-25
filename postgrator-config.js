2ed94701d876b4d48a70983403d08a79aba456c7
Matt Moore <MattMoore@matts-macbook-pro.local>
2021-02-25 17:54:38 -0500

    stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,10 +4,5 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL,
-  "ssl": true,
-  "extra": {
-    "ssl": {
-      "rejectUnauthorized": false,
-    },
-  },
+  "ssl": {"rejectUnauthorized": false}
 }

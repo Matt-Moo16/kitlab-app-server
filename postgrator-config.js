7a960dc43207a7039c947611756787e510d69fa5
Matt Moore <MattMoore@matts-macbook-pro.local>
2021-02-26 23:20:58 -0500

    push for production

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL + "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+  "connectionString": process.env.DATABASE_URL + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
   //"sslmode": process.env.NODE_ENV === "production" ? "require" : "disable"
   "ssl": {
     "require": true,

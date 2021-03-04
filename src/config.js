912bf8900ab764b681b7963b07ea471561ec358a
Matt Moore <MattMoore@matts-macbook-pro.local>
2021-03-04 17:00:26 -0500

    push for production

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/kitlab',
-
+    JWT_SECRET: process.env.JWT_SECRET || 'somenewkey'
 }
